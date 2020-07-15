8d763acf1ca2a948435a5a0e51038f7d442c12c1
Guilherme Brendel <guilhermebrendel@gmail.com>
2020-07-15 10:37:34 -0300

    Simple Character Controller and player movement (Brackeys).

---
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController2D _characterController;
+    [SerializeField] private float _moveSpeed = 40f;
+    private float _horizontalMove;
+    private bool _jump;
+
+    private void Update()
+    {
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * _moveSpeed; //right or d = 1 // left or a = -1
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _characterController.Move(_horizontalMove * Time.fixedDeltaTime, false, _jump);
+        _jump = false;
+    }
+}
+
