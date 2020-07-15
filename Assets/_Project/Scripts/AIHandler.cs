92845b7096db23496f6afcc4237c95c69263bd86
Guilherme Brendel <guilhermebrendel@gmail.com>
2020-07-15 17:10:54 -0300

    Includes simple AI that uses the pattern.

---
+++ Assets/_Project/Scripts/AIHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class AIHandler : MonoBehaviour
+{
+    [SerializeField] private Actor _characterToBeDriven;
+
+    private Command _jumpCommand = new JumpCommand();
+    private Command _moveHorizontalCommand = new MoveHorizontalCommand();
+    private float _moveDirection = 0f;
+    private bool _moveRight;
+
+    private void FixedUpdate()
+    {
+        HandleAI();
+    }
+
+    private void HandleAI()
+    {
+        if (_moveRight)
+        {
+            if (_moveDirection <= 1f)
+            {
+                _moveDirection += Time.fixedDeltaTime * 2f;
+            }
+            else
+            {
+                _moveRight = false;
+            }
+        }
+        else
+        {
+             if (_moveDirection >= -1f)
+            {
+                _moveDirection -= Time.fixedDeltaTime * 2f;
+            }
+            else
+            {
+                _moveRight = true;
+            }
+        }
+ 
+        if (Random.Range(0, 1000) > 990)
+        {
+            _jumpCommand.Execute(_characterToBeDriven);
+        }
+        _moveHorizontalCommand.Execute(_characterToBeDriven, _moveDirection);
+    }
+}
